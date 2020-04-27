bfd2c69be720dce39a2c7ab32d393b19e1b7ed0e
vagrant <vagrant@localhost.localdomain>
2020-04-27 12:20:13 +0000

    Revert "fix"
    This reverts commit 37b391e08e62bef452afe6dc6c82c2f5a6ba81e3.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'homes/top'
   resources :books
   root to: 'homes#top'
-  delete 'books/:id' => 'books#destry'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
